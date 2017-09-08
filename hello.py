76da84afc72ba1f2716c09924a6055798df21ebd
root <post.for.metrika@yandex.ru>
2017-09-09 00:27:10 +0300

    update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def app(environ, start_response):
     response_body = environ['QUERY_STRING'].split('&')
     response = '\n'.join(response_body)
+    response = response.encode()
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(response)))
