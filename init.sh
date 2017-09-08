04747f3dec0faffcb3122b8272e6c81fd41e49fc
root <post.for.metrika@yandex.ru>
2017-09-09 00:15:12 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo git clone https://github.com/papkakrem/stepik_web_project.git /home/box/web
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
