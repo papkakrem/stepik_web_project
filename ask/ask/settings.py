48c2d047e2f6ed0ba1ec7a03e4d475d820454ce6
papkakrem <31762218+papkakrem@users.noreply.github.com>
2017-09-09 23:59:54 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ask_db',
         'USER': 'root',
-        'PASSWORD': 'toor',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
     }
 }
