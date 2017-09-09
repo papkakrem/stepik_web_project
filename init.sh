7819a8c1f222af11477b397e718ba235f0fbddd4
root <post.for.metrika@yandex.ru>
2017-09-09 23:40:05 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+sudo gunicorn -D -c /home/box/web/etc/hello.py hello:app
